1226192b5439494e1a8b535f94c3246e5b1d0cbf
Bartlomiej Zadlo <bartlomiejzadlo@gmail.com>
2019-01-31 20:09:07 +0100

    Added action and mutation to add user to state. Users are stored in array

--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -1,14 +1,25 @@
-const state = {
+import {User} from './user'
+import router from '@/router'
 
+const state = {
+  usersList: []
 }
 const getters = {
 
 }
 const actions = {
-
+  addUserToStorage ({commit}, payload) {
+    if (payload.emailUnique){
+      let user = new User(payload)
+      commit('addUser', user)
+      router.push('/home')
+    }
+  }
 }
 const mutations = {
-
+  addUser (state, user) {
+    state.usersList.push(user)
+  }
 }
 export default {
   namespaced: true,
